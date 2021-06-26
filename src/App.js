3f4ccc9dcd54011d0644d56fa49e3e4a08166cfd
unknown <DionysisP>
2021-06-26 13:18:30 +0300

    import component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React from 'react';
+import ShowData from './ShowData';
 
 function App() {
   return (
     <div className="App">
-      <h1>doctor map</h1>
+      <h1>Doctor Map</h1>
+
+      <ShowData />
+
     </div>
   );
 }
