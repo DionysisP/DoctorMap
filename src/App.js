40ea986e5f87b5ecf96659812dc0b98dc18a5a6d
DionysisP <dion.patsiouras@gmail.com>
2021-06-27 13:08:22 +0300

    Center map on doctor with id 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Data from './data/doctors.json';
 
 function App() {
 
+  var centerLat = Data.results[1].latitude;
+  var centerLong = Data.results[1].longitude;
+
+
+
+
+
   return (
-
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-
+    <React.Fragment>
+      <MapContainer center={[centerLat, centerLong]} zoom={13} scrollWheelZoom={true}>
+        
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[37.9744464, 23.7478837]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </React.Fragment>
   );
 }
 
